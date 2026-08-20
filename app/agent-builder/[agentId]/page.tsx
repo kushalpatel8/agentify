7159317fcf9619939b5bd11fc8097ad7fdf3d452
Kushal Patel <161633671+kushalpatel8@users.noreply.github.com>
2026-08-20 23:18:21 +0530

    Agent Page

---
+++ app/agent-builder/[agentId]/page.tsx
@@ -0,0 +1,45 @@
+'use cleint';
+import React from 'react'
+import Header from '../_components/Header'
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, Background, MiniMap, Controls } from '@xyflow/react';
+import '@xyflow/react/dist/style.css'
+import { useState, useCallback } from 'react';
+
+const initialNodes = [
+    { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+    { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
+function AgentBuilder() {
+
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
+
+    const onNodesChange = useCallback((changes: any) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)), []);
+    const onEdgesChange = useCallback((changes: any) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)), []);
+    const onConnect = useCallback((params: any) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)), []);
+
+    return (
+        <div>
+            <Header />
+            <div style={{ width: '100vw', height: '90vh' }}>
+                <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    onConnect={onConnect}
+                    fitView
+                >
+                    <MiniMap />
+                    <Controls />
+                    {/*@ts-ignore */}
+                    <Background variant='dots' gap={12} size={1}/>
+                </ReactFlow>
+            </div>
+        </div>
+    )
+}
+
+export default AgentBuilder
